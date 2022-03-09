aa55c7fb5d947e85f3590847650a719cc098a578
Eric Sohng <ericsohng@gmail.com>
2022-03-09 15:32:35 -0800

    complete createHTMLBoard() logic

--- connect4.js
+++ connect4.js
@@ -20,141 +20,146 @@ let board = []; // array of rows, each row is array of cells  (board[y][x])
 function makeBoard() {
   // TODO: set "board" to empty HEIGHT x WIDTH matrix array
 
-  for(let y = 0; y < HEIGHT; y++){
+  for (let y = 0; y < HEIGHT; y++) {
     board[y] = [];
-    for(let x = 0; x < WIDTH; x++){
+    for (let x = 0; x < WIDTH; x++) {
       board[y][x] = null;
     }
   }
   console.log(board);
 }
 
-  /** makeHtmlBoard: make HTML table and row of column tops. */
+/** makeHtmlBoard: make HTML table and row of column tops. */
 
-  function makeHtmlBoard() {
-    // TODO: get "htmlBoard" variable from the item in HTML w/ID of "board"
+function makeHtmlBoard() {
+  // TODO: get "htmlBoard" variable from the item in HTML w/ID of "board"
 
-      let htmlBoard = document.getElementById("board")
+  let htmlBoard = document.getElementById("board");
 
-    // TODO: create top row tr that houses individual gameplay buttons
-    let top = document.createElement("tr");
-    top.setAttribute("id", "column-top");
-    top.addEventListener("click", handleClick);
+  // TODO: create top row tr that houses individual gameplay buttons
+  let top = document.createElement("tr");
+  top.setAttribute("id", "column-top");
+  top.addEventListener("click", handleClick);
 
-    // TODO: create individual row position cells and assign to top row
-    
+  // TODO: create individual row position cells and assign to top row
+
+  for (var x = 0; x < WIDTH; x++) {
+    let headCell = document.createElement("td");
+    headCell.setAttribute("id", x);
+    top.append(headCell);
+  }
+  htmlBoard.append(top);
+
+  // dynamically creates the main part of html board
+  // uses HEIGHT to create table rows
+  // uses WIDTH to create table cells for each row
+  for (var y = 0; y < HEIGHT; y++) {
+    // TODO: Create a table row element and assign to a "row" variable
+    let row = document.createElement("tr");
+
     for (var x = 0; x < WIDTH; x++) {
-      let headCell = document.createElement("td");
-      headCell.setAttribute("id", x);
-      top.append(headCell);
+      // TODO: Create a table cell element and assign to a "cell" variable
+      let cell = document.createElement("td");
+
+      // TODO: add an id, y-x, to the above table cell element
+      cell.setAttribute("id", `${y}-${x}`);
+      // you'll use this later, so make sure you use y-x
+
+      // TODO: append the table cell to the table row
+      row.append(cell);
     }
-    htmlBoard.append(top);
+    // TODO: append the row to the html board
+    htmlBoard.append(row);
+  }
+}
 
-    // dynamically creates the main part of html board
-    // uses HEIGHT to create table rows
-    // uses WIDTH to create table cells for each row
-    for (var y = 0; y < HEIGHT; y++) {
-      // TODO: Create a table row element and assign to a "row" variable
+/** findSpotForCol: given column x, return bottom empty y (null if filled) */
 
-      for (var x = 0; x < WIDTH; x++) {
-        // TODO: Create a table cell element and assign to a "cell" variable
+function findSpotForCol(x) {
+  // TODO: write the real version of this, rather than always returning 5
+  return 5;
+}
 
-        // TODO: add an id, y-x, to the above table cell element
-        // you'll use this later, so make sure you use y-x
+/** placeInTable: update DOM to place piece into HTML table of board */
 
-        // TODO: append the table cell to the table row
+function placeInTable(y, x) {
+  // TODO: make a div and insert into correct table cell
+}
 
-      }
-      // TODO: append the row to the html board
+/** endGame: announce game end */
 
-    }
+function endGame(msg) {
+  // TODO: pop up alert message
+}
+
+/** handleClick: handle click of column top to play piece */
+
+function handleClick(evt) {
+  // get x from ID of clicked cell
+  var x = +evt.target.id;
+
+  // get next spot in column (if none, ignore click)
+  var y = findSpotForCol(x);
+  if (y === null) {
+    return;
   }
 
-  /** findSpotForCol: given column x, return bottom empty y (null if filled) */
+  // place piece in board and add to HTML table
+  // TODO: add line to update in-memory board
+  placeInTable(y, x);
 
-  function findSpotForCol(x) {
-    // TODO: write the real version of this, rather than always returning 5
-    return 5;
+  // check for win
+  if (checkForWin()) {
+    return endGame(`Player ${currPlayer} won!`);
   }
 
-  /** placeInTable: update DOM to place piece into HTML table of board */
+  // check for tie
+  // TODO: check if all cells in board are filled; if so call, call endGame
 
-  function placeInTable(y, x) {
-    // TODO: make a div and insert into correct table cell
+  // switch players
+  // TODO: switch currPlayer 1 <-> 2
+}
+
+/** checkForWin: check board cell-by-cell for "does a win start here?" */
+
+function checkForWin() {
+  /** _win:
+   * takes input array of 4 cell coordinates [ [y, x], [y, x], [y, x], [y, x] ]
+   * returns true if all are legal coordinates for a cell & all cells match
+   * currPlayer
+   */
+  function _win(cells) {
+    // TODO: Check four cells to see if they're all legal & all color of current
+    // player
   }
 
-  /** endGame: announce game end */
+  // using HEIGHT and WIDTH, generate "check list" of coordinates
+  // for 4 cells (starting here) for each of the different
+  // ways to win: horizontal, vertical, diagonalDR, diagonalDL
+  for (var y = 0; y < HEIGHT; y++) {
+    for (var x = 0; x < WIDTH; x++) {
+      // TODO: assign values to the below variables for each of the ways to win
+      // horizontal has been assigned for you
+      // each should be an array of 4 cell coordinates:
+      // [ [y, x], [y, x], [y, x], [y, x] ]
 
-  function endGame(msg) {
-    // TODO: pop up alert message
-  }
+      let horiz = [
+        [y, x],
+        [y, x + 1],
+        [y, x + 2],
+        [y, x + 3],
+      ];
+      let vert;
+      let diagDL;
+      let diagDR;
 
-  /** handleClick: handle click of column top to play piece */
-
-  function handleClick(evt) {
-    // get x from ID of clicked cell
-    var x = +evt.target.id;
-
-    // get next spot in column (if none, ignore click)
-    var y = findSpotForCol(x);
-    if (y === null) {
-      return;
-    }
-
-    // place piece in board and add to HTML table
-    // TODO: add line to update in-memory board
-    placeInTable(y, x);
-
-    // check for win
-    if (checkForWin()) {
-      return endGame(`Player ${currPlayer} won!`);
-    }
-
-    // check for tie
-    // TODO: check if all cells in board are filled; if so call, call endGame
-
-    // switch players
-    // TODO: switch currPlayer 1 <-> 2
-  }
-
-  /** checkForWin: check board cell-by-cell for "does a win start here?" */
-
-  function checkForWin() {
-
-    /** _win:
-     * takes input array of 4 cell coordinates [ [y, x], [y, x], [y, x], [y, x] ]
-     * returns true if all are legal coordinates for a cell & all cells match
-     * currPlayer
-     */
-    function _win(cells) {
-
-      // TODO: Check four cells to see if they're all legal & all color of current
-      // player
-
-    }
-
-    // using HEIGHT and WIDTH, generate "check list" of coordinates
-    // for 4 cells (starting here) for each of the different
-    // ways to win: horizontal, vertical, diagonalDR, diagonalDL
-    for (var y = 0; y < HEIGHT; y++) {
-      for (var x = 0; x < WIDTH; x++) {
-        // TODO: assign values to the below variables for each of the ways to win
-        // horizontal has been assigned for you
-        // each should be an array of 4 cell coordinates:
-        // [ [y, x], [y, x], [y, x], [y, x] ]
-
-        let horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];
-        let vert;
-        let diagDL;
-        let diagDR;
-
-        // find winner (only checking each win-possibility as needed)
-        if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) {
-          return true;
-        }
+      // find winner (only checking each win-possibility as needed)
+      if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) {
+        return true;
       }
     }
   }
+}
 
-  makeBoard();
-  makeHtmlBoard();
+makeBoard();
+makeHtmlBoard();
